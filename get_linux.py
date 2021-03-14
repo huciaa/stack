84c09e4f46b022c9371aaf404885ed95479b165a
huciaa <51126637+huciaa@users.noreply.github.com>
2021-03-14 11:21:04 +0100

    Update get_linux.py

--- get_linux.py
+++ get_linux.py
@@ -12,7 +12,23 @@ import shutil
 shutil.register_unpack_format('7zip', ['.7z'], unpack_7zarchive)
 dir_path = '/home/ec2-user'
 files_zipped ='/home/ec2-user/files/'
-lel
+
+def unpack():
+    files = [f for f in os.listdir(files_zipped) if os.path.isfile(files_zipped+f)]
+    for f in files:
+        if ".7z" in f and ".meta." in f and ".tmp" not in f and "stackoverflow" not in f:
+            directory = dir_path+'/unpack/'+f.split('.')[0]+'.meta/'
+
+        elif ".7z" in f and ".tmp" not in f and "stackoverflow" not in f:
+            directory = dir_path+'/unpack/'+f.split('.')[0]+'/'
+        else:
+            continue
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        #Archive(dir_path + '/' + f).extractall(directory)
+        shutil.unpack_archive(files_zipped + '/' + f, directory)
+        print(f+" saved")
+        os.remove(os.path.join(files_zipped, f))
 
 # POBIERANIE DANYCH ZE STRONY
 r = requests.get('https://archive.org/download/stackexchange')
@@ -57,20 +73,6 @@ for index, row in files_to_download.iterrows():
             write = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
             write.writerows(files_downloaded.values.tolist())
         print('{file} saved'.format(file=row[0]))
+    unpack()
 
 
-files = [f for f in os.listdir(files_zipped) if os.path.isfile(files_zipped+f)]
-for f in files:
-    if ".7z" in f and ".meta." in f and ".tmp" not in f and "stackoverflow" not in f:
-        directory = dir_path+'/unpack/'+f.split('.')[0]+'.meta/'
-
-    elif ".7z" in f and ".tmp" not in f and "stackoverflow" not in f:
-        directory = dir_path+'/unpack/'+f.split('.')[0]+'/'
-    else:
-        continue
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    #Archive(dir_path + '/' + f).extractall(directory)
-    shutil.unpack_archive(files_zipped + '/' + f, directory)
-    print(f+" saved")
-    os.remove(os.path.join(files_zipped, f))
