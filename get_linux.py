20b035df1fbaf37ac11b76d38210167a88267a88
EC2 Default User <ec2-user@ip-172-31-24-126.ec2.internal>
2021-03-14 14:10:49 +0000

    c2

--- get_linux.py
+++ get_linux.py
@@ -5,13 +5,18 @@ from bs4 import BeautifulSoup
 import csv
 import wget
 import pandas as pd
-
 import os
+import glob
+from math import ceil
 from py7zr import unpack_7zarchive
 import shutil
+import subprocess
+
 shutil.register_unpack_format('7zip', ['.7z'], unpack_7zarchive)
 dir_path = '/home/ec2-user'
 files_zipped ='/home/ec2-user/files/'
+files_unpacked = '/home/ec2-user/unpack'
+
 
 def unpack():
     files = [f for f in os.listdir(files_zipped) if os.path.isfile(files_zipped+f)]
@@ -29,6 +34,37 @@ def unpack():
         shutil.unpack_archive(files_zipped + '/' + f, directory)
         print(f+" saved")
         os.remove(os.path.join(files_zipped, f))
+
+def run_splitter():
+    files = [f for f in glob.iglob(files_unpacked + '**/**', recursive=True)]
+    for f in files:
+            if os.path.isfile(f):
+                    size =os.stat(f).st_size/(1024*1024)
+                    if int(size) > 100:
+                            split(f)
+                            print("THE FILE "+f+" HAS BEEN SPLITTED")
+def split(file):
+    with open(file, 'r') as f:
+        data = f.readlines()
+    line = 2
+    batch = 1
+    max = 10000
+    batches = ceil(len(data) / 10000)
+    openLine = '<?xml version="1.0" encoding="utf-8"?>\n'
+    openTag = data[1]
+    closeTag = data[-1]
+
+    for a in range(batch, batches + 1, 1):
+        filename= file.replace('.xml', str(a)+'.xml')
+        with open(filename, 'a+') as f:
+            f.write(openLine)
+            f.write(openTag)
+            while line <= a * max and line < len(data) - 1:
+                f.write(data[line])
+                line += 1
+            f.write(closeTag)
+    os.remove(file)
+
 
 # POBIERANIE DANYCH ZE STRONY
 r = requests.get('https://archive.org/download/stackexchange')
@@ -74,5 +110,13 @@ for index, row in files_to_download.iterrows():
             write.writerows(files_downloaded.values.tolist())
         print('{file} saved'.format(file=row[0]))
     unpack()
+    run_splitter()
+    subprocess.call("/home/ec2-user/stack/import.sh")
+
+
+
+
+
+
 
 
